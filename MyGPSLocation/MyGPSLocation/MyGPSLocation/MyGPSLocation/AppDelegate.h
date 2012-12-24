b4c9cef99cd16c20f222cb657e736567fa199df0
cominggithob <misscoming@gmail.com>
2012-12-24 22:43:35 +0800

    commit files

---
+++ MyGPSLocation/MyGPSLocation/MyGPSLocation/MyGPSLocation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyGPSLocation
+//
+//  Created by Coming on 12/12/17.
+//  Copyright (c) 2012年 Coming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
